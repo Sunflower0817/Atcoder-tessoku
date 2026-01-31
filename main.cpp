f5bfe3f1c55d0cc3c574ff6fc2ab64f605f13376
Sunflower0817 <t.s.aug17.2000@gmail.com>
2026-01-31 17:39:54 +0900

    出力先の修正 gitignoreの設定 処理時間計算

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
+#include <fstream>
+#include <chrono>
 using namespace std;
 
 int main() {
+    std::ifstream in("data/input.txt");
+    std::ofstream out("data/output.txt");
+    std::ofstream log("data/log.txt");
+    auto start = std::chrono::high_resolution_clock::now();
+
+    std::cin.rdbuf(in.rdbuf());
+    std::cout.rdbuf(out.rdbuf());
+
     string message;
     cin >> message;
     cout << "Hello, " << message << "!" << endl;
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = end - start;
+    log << "処理時間: " << elapsed.count() << " 秒" << std::endl;
     return 0;
 }
